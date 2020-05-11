5447d6e3b53670e8161b396eea990af489d940a9
Bartosz Michalski <bartek10114@wp.pl>
2020-05-11 20:08:13 +0200

    Poprawki do wpisywania pytan i odpowiedzi

--- Visitare/Visitare/QuestionPage.xaml.cs
+++ Visitare/Visitare/QuestionPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,11 +35,23 @@ namespace Visitare
             {
                 Result = true;
             }
+           
 
             Navigation.PopModalAsync();
         }
+
+
         private async void Button_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                if (String.IsNullOrWhiteSpace(zagadkaEntry.Text) && String.IsNullOrWhiteSpace(odpowiedzEntry1.Text) && String.IsNullOrWhiteSpace(odpowiedzEntry2.Text) && String.IsNullOrWhiteSpace(odpowiedzEntry3.Text) && String.IsNullOrWhiteSpace(odpowiedzEntry4.Text) && String.IsNullOrWhiteSpace(odpowiedzPrawidlowa.Text))
+                {
+                    await DisplayAlert("Błąd", "Uzupełnij wszystkie pola", "Ok");
+                    return;
+                }
+           
+            
             var question = new Question
             {
                 Answers = new List<string> {
@@ -45,8 +59,13 @@ namespace Visitare
                 GoodAnswer = Convert.ToInt16(odpowiedzPrawidlowa.Text),
                 Question1 = zagadkaEntry.Text
             };
+           
+
+           
+
             var questionPage = new QuestionPage(question);
             questionPage.Disappearing += QuestionPageClosed;
+            
 
             await Navigation.PushModalAsync(questionPage);
 
@@ -60,17 +79,31 @@ namespace Visitare
             var content = new StringContent(json, Encoding.UTF8, "application/json");
             HttpClient client = new HttpClient();
             var result = await client.PostAsync("http://dearjean.ddns.net:44301/api/Test4", content);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Błąd", "Uzupełnij wszystkie pola", "Ok");
+            }
         }
 
        private async void QuestionPageClosed(object sender, EventArgs e)
        {
          var questionPage = sender as QuestionPage;
          await DisplayAlert("", "Człowiek odpowiedział: " + (questionPage.Result ? "dobrze" : "źle"), "OK");
+
+            
+
        }
 
         private async void TablicaWynikow(object sender, EventArgs e)
         {
            await Navigation.PushAsync(new Scoreboard());
         }
+
+        private void odpowiedzPrawidlowa_Completed(object sender, EventArgs e)
+        {
+
+        }
+      
     }
 }
